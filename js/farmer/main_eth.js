883e405668ea3024e41962d9b9579af47416ec64
Ferdinand Weinberger <w00lf04@protonmail.com>
2021-10-21 11:19:36 +0200

    Update main_eth.js

--- js/farmer/main_eth.js
+++ js/farmer/main_eth.js
@@ -44,15 +44,9 @@ window.addEventListener('load', async function() {
     }
     
     var prldoc=document.getElementById('playerreflink')
-    prldoc.textContent=window.location.origin+"/index.htm?ref="+currentAddr
+    prldoc.textContent=window.location.origin+"?ref="+currentAddr
     var copyText = document.getElementById("playerreflink");
     copyText.value=prldoc.textContent
-
-    //var ref1 = document.getElementById('ref-link');
-    //var key = CryptoJS.enc.Hex.parse('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f');
-    //var encr = CryptoJS.AES.encrypt(currentAddr, key, { mode: CryptoJS.mode.ECB });
-    //var decr = CryptoJS.AES.decrypt(encr.toString(), key, { mode: CryptoJS.mode.ECB }).toString(CryptoJS.enc.Utf8);
-    //ref1.textContent=window.location.origin+"/BOOMiner/index.html?ref=" + "XX" + encr.toString();
 })
 
 function approve() {
